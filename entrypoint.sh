2d5fdbdc9a16ee9635ba892f7e11c07532216b72
Frank Mayer <gh@frankmayer.dev>
2024-06-30 14:45:45 +0200

    fix: longer sleep for docker deamon

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 # Start Docker daemon in the background
 dockerd &
 
-# Wait for Docker to start
-while (! docker stats --no-stream ); do
+# Wait for Docker to start (wait for `docker stats --no-stream` to exit with 0)
+while ! docker stats --no-stream; do
   echo "Waiting for Docker to start..."
-  sleep 1
+  sleep 4
 done
+echo "Docker is now running!"
 
 # Run your main application
 ./benchmark
